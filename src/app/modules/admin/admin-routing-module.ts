c1337a8629f3b90f129ccc905483ba6db857d83d
GonzaPC - Temperley <gonz.r@hotmail.com>
2026-05-13 20:37:56 -0300

    Se termina de configurar el router secundario

--- src/app/modules/admin/admin-routing-module.ts
+++ src/app/modules/admin/admin-routing-module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './components/home/home';
 
-const routes: Routes = [{
+// Tambien se podria exportar solamente el array de rutas y 
+// asi eliminar codigo innecesario
+
+export const routes: Routes = [{
   path: 'home',
+    component: Home,
   title: 'Admin - Home',
-  component: Home,
+},
+{
+  path: 'config',
+  loadComponent: () => import('./components/config/config').
+    then(m => m.Config),
+  title: 'Admin - Config',
 }
 ];
 
